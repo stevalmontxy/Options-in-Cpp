2ea3922b77d410f777ae4f575890a3b485eb6ff8
yang2323 <sxyang119@gmail.com>
2024-09-07 17:27:52 -0400

    created calculation using historic data

--- Black Scholes/blackscholes_w_volatility_calc.cpp
+++ Black Scholes/blackscholes_w_volatility_calc.cpp
@@ -1,28 +1,37 @@
 /* Black scholes model based on model A in Appendix B of 
 "Option Volatility and Pricing" by Sheldon Natenberg
-This script calculates call and put prices based on static given inputs.
-the other scripts use price data (local historic and live) to calculate volatility
+This script calculates call and put prices based on inputs and historic stock close prices.
+Another script (on the way) uses recent live data to calculate volatility
 last edited: 9/7/24 */
 #include <string>
 #include <iostream>
 #include <bits/stdc++.h> //include log() (natural log) and exp() (e^x) function
 //since log is not constexpr, I can't make it all constexpr :(
 #include <cmath> // include sqrt() and abs() function
+#include <vector> // include vector type
 
+
+double calcVolatility(std::vector<double> closePrice);
 double calcH(double U, double E, double r, double v, double t);
 double NprimeofX(double x);
 double approxNofX(double x);
 double calcCallPrice(double r, double t, double U, double E, double h, double v);
 double calcPutPrice(double r, double t, double U, double E, double h, double v);
 
+
 int main()
 {
-    const double U{100}; // price of the underlying contract
-    const double E{97}; // exercise price
-    const double t{60.0 / 365}; // time to expiration in years (number  of days including weekends/365)
-    const double v{.20}; // annual volatility expressed as a decimal fraction (sigma)
+    std::vector<double> closePrice{548.99, 554.65, 553.78, 541.23, 538.41, 544.44, 544.76, 
+            542.0, 550.81, 543.01, 532.9, 517.38, 522.15, 518.66, 530.65, 532.99, 533.27,
+            542.04, 543.75, 553.07, 554.31, 559.61, 558.7, 560.62, 556.22, 562.13, 560.79, 
+            561.56, 558.3, 558.35, 563.68, 552.08, 550.95, 549.61, 540.36}; // SPY daily adjusted close 7/19/2024 - 9/6/2024
+
+    const double U{closePrice.back()}; // latest price of the underlying contract
+    const double E{535}; // exercise price
+    const double t{20 / 365.0}; // time to expiration in years (number  of days including weekends/365)
     const double r{.04}; // risk-free interested rate expressed as a decimal fraction
 
+    const double v{calcVolatility(closePrice)}; // annual volatility expressed as a decimal fraction (sigma)
     const double h{calcH(U, E, r, v, t)};
 
     std::cout << "For a stock of price $" << U << ", exercise price $" << E << ", time of ~" << t*365 << " days to expiration:\n";
@@ -36,6 +45,28 @@ int main()
     return 0;
 }
 
+double calcVolatility(std::vector<double> closePrice)
+{
+    double vectorLength {closePrice.size()};
+
+    std::vector<double> logPriceChanges;
+        for (int i = 1; i < vectorLength; i++){
+        logPriceChanges.push_back(log(closePrice[i]/closePrice[i-1]));
+    }    
+    
+    double averageLogReturn = std::accumulate(logPriceChanges.begin(), logPriceChanges.end(), 0.0) / (vectorLength - 1);
+
+    std::vector<double> logPriceDevFromMeanSquared;
+    for (int i = 0; i < vectorLength-1; i++){
+        logPriceDevFromMeanSquared.push_back(pow(logPriceChanges[i] - averageLogReturn,2));
+    }
+
+    double stdev {sqrt(std::accumulate(logPriceDevFromMeanSquared.begin(), logPriceDevFromMeanSquared.end(), 0.0)/(vectorLength -2))};
+    double annualizedVolatility {stdev * sqrt(365)};
+
+    std::cout << "Std deviation of log price changes: " << stdev << ", Annualized volatility: " << annualizedVolatility << '\n';
+    return annualizedVolatility;
+}
 
 double calcH(double U, double E, double r, double v, double t)
 {
